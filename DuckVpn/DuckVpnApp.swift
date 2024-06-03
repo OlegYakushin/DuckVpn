5b9d4498daefc08b8d13d95925d99259b87af035
Oleg <yakushinoa@gmail.com>
2024-06-03 13:59:29 +0200

    Initial Commit

---
+++ DuckVpn/DuckVpnApp.swift
@@ -0,0 +1,17 @@
+//
+//  DuckVpnApp.swift
+//  DuckVpn
+//
+//  Created by Oleg Yakushin on 6/3/24.
+//
+
+import SwiftUI
+
+@main
+struct DuckVpnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
